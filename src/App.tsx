ecd9c85ac0055e64c6f6d5db6c90ee15c9aab3ae
paoloswap <122459630+paoloswap@users.noreply.github.com>
2024-09-16 21:35:07 +0200

    Setup Telos Testnet

--- src/App.tsx
+++ src/App.tsx
@@ -14,9 +14,9 @@ const projectId = import.meta.env.VITE_WALLETCONNECT_PROJECT_ID
 
 export const defaultChain = defineChain({
   id: DEFAULT_CHAIN_ID,
-  network: 'holesky',
-  name: 'Holesky',
-  nativeCurrency: { name: 'Holesky Ether', symbol: 'ETH', decimals: 18 },
+  network: 'telos-testnet',
+  name: 'Telos Testnet',
+  nativeCurrency: { name: 'Telos', symbol: 'TLOS', decimals: 18 },
   rpcUrls: {
     default: {
       http: [import.meta.env.VITE_INFURA_RPC],
@@ -27,12 +27,12 @@ export const defaultChain = defineChain({
   },
   blockExplorers: {
     etherscan: {
-      name: 'Etherscan',
-      url: 'https://holesky.etherscan.io',
+      name: 'Teloscan',
+      url: 'https://testnet.teloscan.io',
     },
     default: {
-      name: 'Etherscan',
-      url: 'https://holesky.etherscan.io',
+      name: 'Teloscan',
+      url: 'https://testnet.teloscan.io',
     },
   },
   contracts: {
--- src/constants/default-chain-id.ts
+++ src/constants/default-chain-id.ts
@@ -1,4 +1,4 @@
-export const DEFAULT_CHAIN_ID = 0x4268
-export const DEFAULT_CHAIN_NAME = 'Holesky'
-export const DEFAULT_NATIVE_SYMBOL = 'ETH'
-export const DEFAULT_NATIVE_NAME = 'Ether'
+export const DEFAULT_CHAIN_ID = 0x29
+export const DEFAULT_CHAIN_NAME = 'Telos Testnet'
+export const DEFAULT_NATIVE_SYMBOL = 'WTLOS'
+export const DEFAULT_NATIVE_NAME = 'Telos'
